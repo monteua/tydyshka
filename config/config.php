5b4d87e544b6339a4cf0679ccd800b371162f94a
monteua <arximed.monte@gmail.com>
2020-06-13 19:34:43 +0300

    tiny

--- config/config.php
+++ config/config.php
@@ -3,5 +3,5 @@
 $passwordSalt = "";
 
 define('SALT', $passwordSalt);
-define("BASE_URL", "/profiles/");
-define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/profiles/");
+define("BASE_URL", "/");
+define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/");
